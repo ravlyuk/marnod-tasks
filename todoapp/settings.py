256a65068e7d132f10a3507dd18f0f1258ce4c37
eravlyuk <e.ravlyuk@gethatch.com>
2021-02-14 16:22:47 +0200

    fix port

--- todoapp/settings.py
+++ todoapp/settings.py
@@ -123,7 +123,9 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = os.environ.get("EMAIL_HOST")
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
-EMAIL_PORT = int(os.environ.get("EMAIL_PORT"))
+EMAIL_PORT = os.environ.get("EMAIL_PORT")
+if EMAIL_PORT:
+    EMAIL_PORT = int(EMAIL_PORT)
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = 'default from email'
 
