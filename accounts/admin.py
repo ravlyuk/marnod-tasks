5810292c68927feb89d8922c8373decd20157674
eravlyuk <e.ravlyuk@gethatch.com>
2021-02-14 14:59:39 +0200

    create app

---
+++ accounts/admin.py
@@ -0,0 +1,7 @@
+from accounts.models import Profile
+from django.contrib import admin
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ["user", "birthdate", "avatar"]
---
+++ tasks/static/js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    $(document).on('click', '.checkbox', function () {
+        $(this).parent().addClass('completed');
+        $(this).attr('disabled', true);
+        uid = $(this).attr('data-uid');
+        $.get("/tasks/complete/" + uid);
+        $(location).attr('href', '/tasks/list')
+    });
+    $(document).on('click', '.remove', function () {
+        $(this).parent().remove();
+    });
+});
