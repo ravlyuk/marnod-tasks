997a54278184ab6ff1825cbd5c6b22b9f6997f24
eravlyuk <e.ravlyuk@gethatch.com>
2021-03-09 01:34:45 +0200

    env

--- todoapp/settings.py
+++ todoapp/settings.py
@@ -112,11 +112,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = '/staticfiles'
+STATIC_URL = '/staticfiles/'
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
-MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
+MEDIA_URL = "/mediafiles/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "mediafiles")
 
 LOGIN_REDIRECT_URL = "tasks:list"
 
