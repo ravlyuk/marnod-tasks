8d130c2c7c95a97834a2e628126a18099ba9dc87
eravlyuk <e.ravlyuk@gethatch.com>
2021-03-08 19:48:13 +0200

    env

--- todoapp/settings.py
+++ todoapp/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['0.0.0.0', '3.10.213.183']
 
