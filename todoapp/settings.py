21a3a153e4aae92298003b579a6f0dc58a4bd3ea
eravlyuk <e.ravlyuk@gethatch.com>
2021-03-08 18:47:40 +0200

    env

--- todoapp/settings.py
+++ todoapp/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['3.10.213.183']
 
 # Application definition
 
