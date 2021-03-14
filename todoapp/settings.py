9635c93c972eeef7fc528b4728c5972acbbb2d71
eravlyuk <e.ravlyuk@gethatch.com>
2021-03-15 00:19:32 +0200

    +

--- todoapp/settings.py
+++ todoapp/settings.py
@@ -4,10 +4,11 @@ from pathlib import Path
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
