7d20a6e4a6eacce9ed9a6ed77f528cf0a5db0bb5
eravlyuk <e.ravlyuk@gethatch.com>
2021-04-10 21:24:55 +0300

    env

---
+++ todoapp/prod_settings.py
@@ -0,0 +1,16 @@
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+SECRET_KEY = os.environ.get("SECRET_KEY", "4%w^0ig86f1l^rп2rn3-e8gk9bjn0j9ag(v!6x-*wde7m!")
+
+DEBUG = False
+
+ALLOWED_HOSTS = ['0.0.0.0', '3.8.4.66']
+
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [STATIC_DIR]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+# https://youtu.be/mp4rwP7Ny_A?t=5557
