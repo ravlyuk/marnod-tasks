65d3867d991c42722624c3702f489130965369d3
eravlyuk <e.ravlyuk@gethatch.com>
2021-04-10 21:36:46 +0300

    env

--- todoapp/prod_settings.py
+++ todoapp/prod_settings.py
@@ -9,7 +9,7 @@ DEBUG = False
 ALLOWED_HOSTS = ['0.0.0.0', '172.31.31.141']
 
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [STATIC_DIR]
+# STATICFILES_DIRS = [STATIC_DIR]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
