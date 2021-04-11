940ff4798236f88f7c4841e98b61c7ef45876d73
eravlyuk <e.ravlyuk@gethatch.com>
2021-04-11 16:05:18 +0300

    env

--- todoapp/prod_settings.py
+++ todoapp/prod_settings.py
@@ -4,7 +4,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = os.environ.get("SECRET_KEY", "4%w^0ig86f1l^rп2rn3-e8gk9bjn0j9ag(v!6x-*wde7m!")
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['0.0.0.0', '35.178.235.111']
 
