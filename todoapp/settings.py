9fd15c1be35a3d774f7abf66e64035f7ad614369
eravlyuk <e.ravlyuk@gethatch.com>
2021-04-14 15:19:01 +0300

    todo

--- todoapp/settings.py
+++ todoapp/settings.py
@@ -63,10 +63,21 @@ WSGI_APPLICATION = 'todoapp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'yerav',
+        'USER': 'yerav',
+        'PASSWORD': 'Za2leyhzjkrx@',
+        'HOST': 'todo_db',
+        'PORT': 5432
     }
 }
 
