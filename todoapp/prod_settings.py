49508ec8610e6a91104aacbaae434459b97315b7
eravlyuk <e.ravlyuk@gethatch.com>
2021-04-14 10:59:42 +0300

    todo

--- todoapp/prod_settings.py
+++ todoapp/prod_settings.py
@@ -6,7 +6,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "4%w^0ig86f1l^rп2rn3-e8gk9bjn0j9ag(v!
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['3.10.212.247', '127.0.0.1', 'localhost', '0.0.0.0']
+ALLOWED_HOSTS = ['35.176.120.164', '127.0.0.1', 'localhost', '0.0.0.0']
 
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
 # STATICFILES_DIRS = [STATIC_DIR]
